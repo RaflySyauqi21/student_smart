4c51dc27b2b47f409eddfd0dc34d8e22785d6706
RaflySyauqi21 <raflysyauqi2122@gmail.com>
2025-05-17 16:17:17 +0700

    bahasa indonesia page

---
+++ lib/pages/bahasa_indonesia_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BahasaIndonesiaPage extends StatelessWidget {
+  const BahasaIndonesiaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text("Bahasa Indonesia"));
+  }
+}
--- lib/pages/quiz_selection_page.dart
+++ lib/pages/quiz_selection_page.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
 
-class QuizSelectionpage extends StatelessWidget {
+class QuizSelectionpage extends StatefulWidget {
   const QuizSelectionpage({super.key});
 
   @override
+  State<QuizSelectionpage> createState() => _QuizSelectionpageState();
+}
+
+class _QuizSelectionpageState extends State<QuizSelectionpage> {
+  List<bool> matapelajaran = [false, false, false, false, false];
+  List<String> namaMataPelajaran = [
+    'Bahasa Indonesia',
+    'Matematika',
+    'Bahasa Inggris',
+    'Pendidikan Agama',
+    'Seni Budaya',
+  ];
+  @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
       color: Color.fromRGBO(157, 206, 237, 1),
-      child: const Center(child: Column(children: [Text("Tahukah Kamu")])),
+      child: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(93, 169, 217, 1),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Text("Quiz", style: TextStyle(fontSize: 25)),
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: ListView.builder(
+                  itemCount: namaMataPelajaran.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      margin: EdgeInsets.symmetric(vertical: 15),
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: Color.fromRGBO(93, 169, 217, 1),
+                      ),
+                      child: Text(
+                        namaMataPelajaran[index],
+                        style: TextStyle(fontSize: 24),
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
