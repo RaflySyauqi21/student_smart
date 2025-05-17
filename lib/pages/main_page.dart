874d46defa274c6d9299de52cd4d32d603aeefc5
RaflySyauqi21 <raflysyauqi2122@gmail.com>
2025-05-17 14:43:42 +0700

    isian home page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -6,8 +6,47 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25),
       color: Color.fromRGBO(157, 206, 237, 1),
-      child: const Center(child: Column(children: [Text("Tahukah Kamu")])),
+      child: Center(
+        child: Column(
+          spacing: 50,
+          children: [
+            Text("Tahukah Kamu?", style: TextStyle(fontSize: 50)),
+            Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(93, 169, 217, 1),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Column(
+                spacing: 30,
+                children: [
+                  Text(
+                    "Otak Kita Bekerja Lebih Baik",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Text(
+                    "Saat Kita Belajar Dengan",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Text(
+                    "Suasana Hati Yang Senang",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 1),
+            Text(
+              '“Ing Ngarsa Sung Tulada Ing Madya Mangun Karsa Tut Wuri Handayani”',
+              style: TextStyle(fontSize: 25),
+              textAlign: TextAlign.center,
+            ),
+            Text("Ki Hajar Dewantara", style: TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
     );
   }
 }
